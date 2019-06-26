7d96b39c7c354406199979acaea2b9c694d9148e
Piotrekf3 <piotrek.fur@wp.pl>
2019-06-26 19:24:59 +0200

    Add NNET and SVM

--- compile.sh
+++ compile.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
+
+#compile and register
+
 /nz/export/ae/utilities/bin/compile_ae --echo --language r \
---version 3 --template compile --db $1 forecast.r
+--version 3 --template compile --db $1 forecastBayes.r
 
 /nz/export/ae/utilities/bin/register_ae --language r --version 3 \
---template udtf --exe forecast.r --sig "PUT_FORECAST(VARARGS)" \
+--template udtf --exe forecastBayes.r --sig "PUT_FORECAST_BAYES(VARARGS)" \
 --return "TABLE(predicted_class VARCHAR(1024))" --db $1
+
+/nz/export/ae/utilities/bin/register_ae --language r --version 3 \
+--template udtf --exe forecastBayes.r --sig "PUT_FORECAST_BAYES_SEQ(VARARGS)" \
+--return "TABLE(predicted_class VARCHAR(1024))" --db $1 --noparallel
+
+/nz/export/ae/utilities/bin/compile_ae --echo --language r \
+--version 3 --template compile --db $1 forecastSVM.r
+
+/nz/export/ae/utilities/bin/register_ae --language r --version 3 \
+--template udtf --exe forecastSVM.r --sig "PUT_FORECAST_SVM(VARARGS)" \
+--return "TABLE(predicted_class VARCHAR(1024))" --db $1
+
+/nz/export/ae/utilities/bin/register_ae --language r --version 3 \
+--template udtf --exe forecastSVM.r --sig "PUT_FORECAST_SVM_SEQ(VARARGS)" \
+--return "TABLE(predicted_class VARCHAR(1024))" --db $1 --noparallel
+
+/nz/export/ae/utilities/bin/compile_ae --echo --language r \
+--version 3 --template compile --db $1 forecastNNET.r
+
+/nz/export/ae/utilities/bin/register_ae --language r --version 3 \
+--template udtf --exe forecastNNET.r --sig "PUT_FORECAST_NNET(VARARGS)" \
+--return "TABLE(predicted_class VARCHAR(1024))" --db $1
+
+/nz/export/ae/utilities/bin/register_ae --language r --version 3 \
+--template udtf --exe forecastNNET.r --sig "PUT_FORECAST_NNET_SEQ(VARARGS)" \
+--return "TABLE(predicted_class VARCHAR(1024))" --db $1 --noparallel
---
+++ forecastNNET.r
@@ -0,0 +1,16 @@
+nz.fun <- function() {
+	install.packages('/nz/export/ae/applications/nnet_x.x.tar.gz', repos=NULL, type='source',INSTALL_opts = c('--no-lock'))
+	library(nnet)
+	model <- readRDS('/nz/export/ae/applications/modelNNET.rds')
+	while(getNext()) {
+		daylength <- getInputColumn(0)
+		season <- getInputColumn(1)
+		
+		data <- data.frame(V3 = daylength, V4 = season)
+		fcast <- predict(model, data)
+		
+		setOutputString(0, fcast)
+		outputResult()
+	}
+
+}
